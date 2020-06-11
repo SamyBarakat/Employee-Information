102e95d04bd42cd3505df068582b2a12b50db674
SamyBarakat <sbarakat2@mail.sfsu.edu>
2020-06-10 21:56:57 -0700

    Main Class

--- EmployeeApp.java
+++ EmployeeApp.java
@@ -10,7 +10,6 @@ public class EmployeeApp {
     public static void main(String[] args) {
         
         // Constructed Object
-        
         Employee em1 = new Employee("Samy", "Barakat");
         System.out.println("----------------------");
         System.out.println("Reduced Information:\n----------------------");
